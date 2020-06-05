a73674b738beedf0b9e0fbd23f27ebab37258241
Tanvir Mahmud Dheman <64571786+tmdheman@users.noreply.github.com>
2020-06-05 12:48:50 +0600

    Update speakGoodbye.js

--- ass-mod-4/speakGoodbye.js
+++ ass-mod-4/speakGoodbye.js
@@ -1,7 +1,7 @@
 (function  (window) {
 	var byeSpeaker = {};
 	var speakWord = "Good Bye";
-	byeSpeaker.speak=function(name) {
+	byeSpeaker.speak=function speak(name) {
   console.log(speakWord + " " + name);
 }
 
